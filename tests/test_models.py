2d07139b5146e024870af4f5f9060618af29c7f4
Toby Dacre <toby.junk@gmail.com>
2023-06-29 15:28:39 +0100

    4. email -> lower case test added

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,3 +8,5 @@ class FeedbackModelTest(TestCase):
         feedback = models.Feedback(email="Test@Example.Com", content="Some feedback")
         feedback.save()
         self.assertEqual(feedback.content, "Some feedback")
+        # emails should be lower case
+        self.assertEqual(feedback.email, "test@example.com")
