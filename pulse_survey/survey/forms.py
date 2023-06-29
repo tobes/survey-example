47fe8ace6d0914b704923d35666cc204e9e9741a
Toby Dacre <toby.junk@gmail.com>
2023-06-29 15:26:30 +0100

    2. email validation - also made email compulsary not sure if that is correct approach

--- pulse_survey/survey/forms.py
+++ pulse_survey/survey/forms.py
@@ -4,13 +4,16 @@ from django.core.exceptions import ValidationError
 
 
 def is_cabinet_office_email(email_address):
-    # TODO - update to raise a ValidationError for non-Cabinet Office emails
+    # quick and dirty test
+    # we don't care if is is a real email address as long as it looks near
+    # enough.
+    if not email_address.endswith('@cabinetoffice.gov.uk'):
+        raise ValidationError('Must be a Cabinet Office email address')
     return True
-    
 
 
 class FeedbackForm(forms.Form):
     template_name = "feedback.html"
-    email = forms.EmailField(required=False, validators=[is_cabinet_office_email])
+    email = forms.EmailField(required=True, validators=[is_cabinet_office_email])
     content = forms.TextInput()
 
